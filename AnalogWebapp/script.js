3b23b3561b3c1400df773fc97ca262e770e64c8f
Nipun Sandeepa Karunarathana <123877192+karunarathana@users.noreply.github.com>
2024-07-03 11:39:19 +0530

    Add files via upload

---
+++ AnalogWebapp/script.js
@@ -0,0 +1,21 @@
+function setClock() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondHand = document.getElementById('second');
+    const minuteHand = document.getElementById('minute');
+    const hourHand = document.getElementById('hour');
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setClock, 1000);
+setClock();
